d0183d1466f20acd600d6166c99465e5a704f2f4
demystik <demystiktech@gmail.com>
2023-04-22 11:37:00 +0100

    8-concat_edges.py

---
+++ 0x00/8-concat_edges.py
@@ -0,0 +1,5 @@
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str1 = str[39:67]
+str2 = str[-22:-17]
+print(f"{str1}{str2}{str[0:6]}")
