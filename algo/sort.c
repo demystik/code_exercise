44500d5137646f16f395d4023e4e96393e1af70f
demystik <demystiktech@gmail.com>
2023-04-22 11:22:31 +0100

    testing

---
+++ algo/sort.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+int arr[argc];
+int num;
+time_t t1;
+srand((unsigned)time(&t1));
+num = rand() % 100;
+
+printf("randomNum is %d", num);
+
+return (0);
+}
