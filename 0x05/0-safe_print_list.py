ba423f38d81d79b6906b82f257ef75a17e333f8f
demystik <demystiktech@gmail.com>
2023-04-24 22:35:11 +0100

    0-safe_print_list.py

---
+++ 0x05/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    num = 0
+    try:
+        for i in range(x):
+            print(f"{my_list[i]}", end="")
+            num = num + 1
+    except:
+        pass
+    print("")
+    return num
