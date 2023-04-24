4b7854a5a818c8958b86be74fc6535203a9150f2
demystik <demystiktech@gmail.com>
2023-04-24 22:36:16 +0100

    2-safe_print_list_integers.py

---
+++ 0x05/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
+                num = num + 1
+    except ValueError:
+        pass
+    print("")
+    return num
