75b18207ffd5ea393e2b86efa5713cfebf9525ba
demystik <demystiktech@gmail.com>
2023-07-03 06:39:31 +0100

    10_trick.py

--- 10_trick.py
+++ 10_trick.py
@@ -2,10 +2,20 @@
 
 print([i * i for i in range(10) if (i % 2 == 0)])
 
+
+
 condition = True
 x = 1 if condition else 0
+
+
 
 num1 = 100_000_000_000
 num2 = 1_000_000_000
 total = num1 + num2
 print(f'{total:,}')
+
+
+with open('myFile.txt', 'r') as file:
+	fileContent = file.read()
+words = fileContent.split(' ')
+print(len(words))
