a864ecfad45ae5af1e6d97d6087aec8fab9b52b2
demystik <demystiktech@gmail.com>
2023-07-03 06:36:15 +0100

    10_trick.py

--- 10_trick.py
+++ 10_trick.py
@@ -4,3 +4,8 @@ print([i * i for i in range(10) if (i % 2 == 0)])
 
 condition = True
 x = 1 if condition else 0
+
+num1 = 100_000_000_000
+num2 = 1_000_000_000
+total = num1 + num2
+print(f'{total:,}')
