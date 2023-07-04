1ff0c189eeefd4187cd39feafc4e27b7909bd833
demystik <demystiktech@gmail.com>
2023-07-04 07:30:11 +0100

    task 1 ex

--- 50DaysOfPythonChallenge/1_Day_ex.py
+++ 50DaysOfPythonChallenge/1_Day_ex.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
 def longest_value(dic):
+    """function called longest_value that takes a dictionary
+    as an argument and returns the first longest value of the
+    dictionary
+    """
+
     diction = [ val for val in dic.values()]
     diction.sort(reverse=True)
     print(diction[0])
+
+
 
 fruits = {'fruit': 'apple', 'color': 'green'}
 longest_value(fruits)
