177888bfd38c619a36f3d95f185db331d57bbbcd
demystik <demystiktech@gmail.com>
2023-07-04 07:14:13 +0100

    task 1

--- 50DaysOfPythonChallenge/1_Day.py
+++ 50DaysOfPythonChallenge/1_Day.py
@@ -2,7 +2,13 @@
 
 import math
 def divide_or_square(num):
-    return math.sqrt(num) if num % 5 == 0 else num % 5
+    """takes one
+    argument (a number), and returns the square root of the number
+    if it is divisible by 5, returns its remainder if it is not divisible by
+    5. For example, if you pass 10 as an argument, then your function
+    should return 3.16 as the square root.
+    """
+    return f'{math.sqrt(num):.2f}' if num % 5 == 0 else num % 5
 
 number = 10
 print(divide_or_square(number))
