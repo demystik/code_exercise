0ec295e1b57595ddf9b94b548bd5c0031d402483
demystik <demystiktech@gmail.com>
2023-07-05 17:51:27 +0100

    task 3

--- 50DaysOfPythonChallenge/3_Day.py
+++ 50DaysOfPythonChallenge/3_Day.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def register_check(register):
+    """register_check that checks how many
+    students are in school. The function takes a dictionary as a
+    parameter. If the student is in school, the dictionary says ‘yes’. If
+    the student is not in school, the dictionary says ‘no’. Your
+    function should return the number of students in school. Use the
+    dictionary below. Your function should return 3
+    """
     presence = 0
     for val in register.values():
         if val == 'yes':
