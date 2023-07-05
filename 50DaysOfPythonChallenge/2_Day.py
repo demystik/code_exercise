b3578b5bc888de41798fd2a8b14abdad70d21456
demystik <demystiktech@gmail.com>
2023-07-05 17:49:12 +0100

    task 2

--- 50DaysOfPythonChallenge/2_Day.py
+++ 50DaysOfPythonChallenge/2_Day.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def convert_add(List):
+    """convert_add that takes a list of strings
+    as an argument and converts it to integers and sums the list. For
+    example [‘1’, ‘3’, ‘5’] should be converted to [1, 3, 5] and
+    summed to 9.
+    """
     total = 0
     for num in List:
         total += int(num)
