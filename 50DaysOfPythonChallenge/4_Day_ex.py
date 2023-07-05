db49170defad6ba7f710d2b6fbdb32a2cd1fcece
demystik <demystiktech@gmail.com>
2023-07-05 17:53:49 +0100

    task 4 ex

--- 50DaysOfPythonChallenge/4_Day_ex.py
+++ 50DaysOfPythonChallenge/4_Day_ex.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def word_index(List):
+    """word_index that takes one argument,
+    a list of strings and returns the index of the longest word in the
+    list. If there is no longest word (if all the strings are of the same
+            length), the function should return zero (0). For example, the list
+    below should return 2.
+    """
     copyList = List.copy()
     List.sort(reverse=True)
     for word in range(len(List)):
