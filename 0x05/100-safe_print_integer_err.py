533a205f28e37215e8d56973e9b6cf4e610c9f47
demystik <demystiktech@gmail.com>
2023-07-16 10:45:25 +0100

    Update

--- 0x05/100-safe_print_integer_err.py
+++
@@ -1,9 +0,0 @@
-import sys
-def safe_print_integer_err(value):
-	try:
-		print("{:d}".format(value))
-		return True
-	except ValueError as verr:
-		print(f"Exception",end="")
-		sys.stderr.write(str(verr))
-	return False
