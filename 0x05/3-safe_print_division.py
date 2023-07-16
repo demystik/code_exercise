0955a1ebf1899aedc74577c52ce56b90eb8dba8f
demystik <demystiktech@gmail.com>
2023-07-16 10:45:43 +0100

    Update

--- 0x05/3-safe_print_division.py
+++
@@ -1,9 +0,0 @@
-def safe_print_division(a, b):
-	result = None
-	try:
-		result = a/b
-	except ZeroDivisionError:
-		return None
-	finally:
-		print("Inside result: {}".format(result))
-		return result
