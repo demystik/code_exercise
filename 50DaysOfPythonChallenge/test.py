ca30d5d679a0f3d414182af0eea28f3462dcf9f2
demystik <demystiktech@gmail.com>
2023-07-31 22:12:08 +0100

    5o days

--- 50DaysOfPythonChallenge/test.py
+++ 50DaysOfPythonChallenge/test.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-names = ["kerry", "dickson","ade","sola","ade","sola","John", "Mary","carol", "Rose", "adam"]
-seg_name = []
-seg_nam = [seg_name.append(name) for name in names if name not in seg_name]
-#for name in names:
-#    if name not in seg_name:
-#        seg_name.append(name)
-print(seg_name)
+
+num = "3452335"
+for i in num:
+    if int(i) % 2 == 1:
+        print(i)
